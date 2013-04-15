2e200bb5408081b96e90789c6bf58f0f62f59a56
Ben Weissmann <benweissmann@gmail.com>
2013-04-15 17:07:20 -0400

    finish up recipe pane

--- js/recipe-pane.js
+++ js/recipe-pane.js
@@ -7,10 +7,23 @@
 
     // given a (title, url) tuple, returns an <a> tag
     function makeLink(params) {
-        return "<a href=\"" + params[1] + "\">" + params[0] + "</a>";
+        return "<a href=\"" + params[1] + "\" target=\"_blank\">" + params[0] + "</a>";
+    }
+
+    // given directions and a list of ingredients, produces substituted
+    // directions
+    function subDirections(directions, ingreds) {
+        ingreds.forEach(function(ingred) {
+            if(ingred.substitute) {
+                directions = directions.replace(new RegExp(ingred.original[1], 'g'), ingred.substitute[1])
+            }
+        });
+
+        return directions
     }
 
     var RecipePane = {
+        // displays the recipe with the given name
         display: function(name) {
             var recipe = RECIPE_PANE_DATA[name];
 
@@ -29,20 +42,23 @@
                     listItem.append('<span class="ingred-orig">' + formatIngred(ingredient.original));
                     listItem.append('<span class="ingred-sub">' + formatIngred(ingredient.substitute));
 
-                    listItem.popover({
+                    var btn = $("<button class=\"btn btn-small\"><i class=\"icon-question-sign\"></i></button>")
+
+                    btn.popover({
                         html: "true",
                         placement: "right",
-                        trigger: "manual ",
+                        trigger: "hover ",
                         title: ingredient.original[1],
-                        delay: { hide: 100 },
-                        content: ("<b>substitution:</b>" + formatIngred(ingredient.substitute) + "<br />" +
-                                 "<b>where to buy:</b>" + makeLink(ingredient.where_to_buy) + "<br />" +
-                                 "<b>alternatives:</b>" + _.map(ingredient.other_substitutions, function(sub) {
+                        delay: { hide: 250 },
+                        container: btn[0],
+                        content: ("<b>substitution: </b>" + formatIngred(ingredient.substitute) + "<br />" +
+                                 "<b>where to buy: </b>" + makeLink(ingredient.where_to_buy) + "<br />" +
+                                 "<b>alternatives: </b>" + _.map(ingredient.other_substitutions, function(sub) {
                                      return formatIngred(sub);
                                  }).join(", "))
                     });
 
-                    listItem.addClass("has-details");
+                    listItem.append(btn);
                 }
                 else {
                     listItem = $("<li>" + formatIngred(ingredient.original) + "</li>");
@@ -51,9 +67,21 @@
                 $("#recipe-ingreds").append(listItem);
             });
             
-            $("#recipe-directions").text(recipe.directions);   
+            $("#recipe-directions-orig").text(recipe.directions);
+            $("#recipe-directions-sub").text(subDirections(recipe.directions, recipe.ingredients));   
         }
     }
+
+    // set up original / substituted buttons
+    $(function() {
+        $("#recipe-show-orig").click(function() {
+            $("#recipe-pane").removeClass("substituted").addClass("original");
+        });
+
+        $("#recipe-show-sub").click(function() {
+            $("#recipe-pane").removeClass("original").addClass("substituted");
+        });
+    })
 
     exports.RecipePane = RecipePane;
 })(window)
