b2fcb4979d9f249dcf22d37ce62a920eece06993
Miren Bamforth <miren@mit.edu>
2013-04-17 02:08:47 -0400

    results pane first commit, added a new recipe

---
+++ data/more-recipe-pane.js
@@ -0,0 +1,47 @@
+MORE_RECIPE_PANE_DATA = {
+    "Vegan Banana Pancakes": {
+        "source": ["Now Stir It Up", "http://nowstiritup.blogspot.com/2013/03/vegan-banana-pancakes.html"],
+        "time": "30 mins",
+        "difficulty": "easy",
+        "ingredients": [
+            {
+                "original": ["1 tbsp", "granulated sugar"]
+            },
+            {
+                "original": ["1.5 cups", "unbleached all-purpose flour"]
+            },
+            {
+                "original": ["1/2 tsp", "salt"]
+            },
+            {
+                "original": ["1 cup", "milk"],
+                "substitute": ["1 cup", "soy milk"],
+                "where_to_buy": ["Shaw's", "http://www.shaws.com/stores/search-stores.html"],
+                "other_substitutions": [
+                    ["1 cup", "rice milk"],
+                    ["1 cup", "almond milk"],
+                    ["1 cup", "coconut milk"]
+                ]
+            },
+            {
+                "original": ["1 tsp", "cinnamon"]
+            },
+            {
+                "original": ["2 tsp", "baking powder"]
+            },
+            {
+                "original": ["1", "egg"],
+                "substitute": ["1 cup", "mashed bananas"],
+                "where_to_buy": ["Shaw's", "http://www.shaws.com/stores/search-stores.html"],
+                "other_substitutions": [
+                    ["1/2 cup", "textured vegitable protein"],
+                    ["1 tbsp", "nutritional yeast"]
+                ]
+            },
+            {
+                "original": ["3 tbsp", "corn oil"]
+            }
+        ],
+        "directions": "Pour the flour, salt, granulated sugar, cinnamon, and baking powder in a large bowl and combine. Mash the bananas. Mix in the bananas and soy milk. Stir until combined. Let the pancake batter sit for 5 minutes before using. Pre-heat oven to 200 degrees. Heat a large skillet with 1-2 Tbsp of the oil over medium heat. Cook until bubbles begin to form around the edge of the pancakes. Flip and cook for another 2-3 minutes. Add remaning oil as needed and continue to cook pancakes until all the batter is gone."
+    }
+}
---
+++ js/results-pane.js
@@ -0,0 +1,31 @@
+(function(exports) {
+
+    // creates the contents of a div that contains the name, time, and difficulty of a search result
+    // name is the title of the recipe, counter is to give the div an id with the correct counter
+    function createResult(name, counter) {
+        var recipe;
+        if (counter == 0) {
+            recipe = RECIPE_PANE_DATA[name];
+        } else {
+            recipe = MORE_RECIPE_PANE_DATA[name];
+        }
+        
+        var div_string = "<div id=\"result" + counter + "\" class=\"result\"><h4>"+ name + "</h4><ul><li>";
+        div_string = div_string + recipe.time +"</li><li>" + recipe.difficulty +"</li></ul></div>";
+        return div_string;
+    }
+
+    var ResultsPane = {
+        // displays the results
+        display: function(search_results) {
+            // loops through the results making a div for each one
+            for(var i = 0; i < search_results.length; i++) {
+                console.log(search_results[i]);
+                var new_div = createResult(search_results[i], i);
+                $("#recipe-results").append(new_div);
+            }
+        }
+    }
+
+    exports.ResultsPane = ResultsPane;
+})(window)
